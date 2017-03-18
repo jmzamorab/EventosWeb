fe688066dfb6be1b758d2026516f664408798b12
jmzamorab <josem.zamorab@gmail.com>
2017-03-18 14:27:14 +0100

    Ahora con webView

--- app/src/main/java/es/upv/master/eventosweb/ActividadPrincipal.java
+++ app/src/main/java/es/upv/master/eventosweb/ActividadPrincipal.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class ActividadPrincipal extends AppCompatActivity{
     WebView navegador;
@@ -14,13 +16,23 @@ public class ActividadPrincipal extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actividad_principal);
         navegador = (WebView) findViewById(R.id.webkit);
-        //navegador.loadUrl("https://eventos-d4f6a.firebaseapp.com/index.html");
-
         navegador.getSettings().setJavaScriptEnabled(true);
-        navegador.loadUrl("file:///android_asset/index.html");
+        navegador.getSettings().setBuiltInZoomControls(false);
+        navegador.loadUrl("https://eventos-d4f6a.firebaseapp.com/index.html");
+
+
+        //navegador.loadUrl("file:///android_asset/index.html");
         //Intent intent = new Intent(Intent.ACTION_VIEW);
         //Uri uri = Uri.parse( "https://eventos-d4f6a.firebaseapp.com/index.html");
         //intent.setData(uri);
         //startActivity(intent);
+
+        navegador.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request)
+            {
+                return false;
+            }
+        });
     }
 }
