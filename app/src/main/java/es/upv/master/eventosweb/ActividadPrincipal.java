e4a70c093fd5ce8ffb10b6f0237fce028bee23a8
jmzamorab <josem.zamorab@gmail.com>
2017-03-18 11:23:23 +0100

    Ahora con webView

--- app/src/main/java/es/upv/master/eventosweb/ActividadPrincipal.java
+++ app/src/main/java/es/upv/master/eventosweb/ActividadPrincipal.java
@@ -4,15 +4,20 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class ActividadPrincipal extends AppCompatActivity{
+    WebView navegador;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actividad_principal);
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        Uri uri = Uri.parse( "https://eventos-d4f6a.firebaseapp.com//index.html");
-        intent.setData(uri);
-        startActivity(intent);
+        navegador = (WebView) findViewById(R.id.webkit);
+        navegador.loadUrl("https://eventos-d4f6a.firebaseapp.com/index.html");
+        //Intent intent = new Intent(Intent.ACTION_VIEW);
+        //Uri uri = Uri.parse( "https://eventos-d4f6a.firebaseapp.com/index.html");
+        //intent.setData(uri);
+        //startActivity(intent);
     }
 }
