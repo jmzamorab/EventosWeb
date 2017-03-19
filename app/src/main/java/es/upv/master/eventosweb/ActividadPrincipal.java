2885117e9b2c0d8ef45167065d9cfb30afbd2b17
jmzamorab <josem.zamorab@gmail.com>
2017-03-19 10:55:45 +0100

    Descarga de ficheros, captura aslertas JS, gestión de errores ... Voy por probar conectividad pg 23

--- app/src/main/java/es/upv/master/eventosweb/ActividadPrincipal.java
+++ app/src/main/java/es/upv/master/eventosweb/ActividadPrincipal.java
@@ -1,12 +1,18 @@
 package es.upv.master.eventosweb;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.DownloadListener;
+import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -15,7 +21,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
-public class ActividadPrincipal extends AppCompatActivity{
+import static android.R.id.message;
+
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ActividadPrincipal extends AppCompatActivity {
     private ProgressBar barraProgreso;
     ProgressDialog dialogo;
     WebView navegador;
@@ -41,8 +60,7 @@ public class ActividadPrincipal extends AppCompatActivity{
         btnGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (edtHttp.getText() != null)
-                {
+                if (edtHttp.getText() != null) {
                     navegador.loadUrl(edtHttp.getText().toString());
                 }
             }
@@ -56,14 +74,27 @@ public class ActividadPrincipal extends AppCompatActivity{
 
         navegador.setWebViewClient(new WebViewClient() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request)
-            {
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 return false;
             }
         });
         barraProgreso = (ProgressBar) findViewById(R.id.barraProgreso);
 
         navegador.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message,
+                                     final JsResult result) {
+                new AlertDialog.Builder(ActividadPrincipal.this).setTitle("Mensaje")
+                        .setMessage(message).setPositiveButton
+                        (android.R.string.ok, new AlertDialog.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                result.confirm();
+                            }
+                        }).setCancelable(false).create().show();
+                return true;
+            }
+
+
             @Override
             public void onProgressChanged(WebView view, int progreso) {
                 barraProgreso.setProgress(0);
@@ -76,7 +107,41 @@ public class ActividadPrincipal extends AppCompatActivity{
             }
         });
 
-        navegador.setWebViewClient(new WebViewClient(){
+        navegador.setDownloadListener(new DownloadListener() {
+            public void onDownloadStart(final String url, String userAgent, String contentDisposition, String mimetype,long contentLength) {
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(ActividadPrincipal.this);
+                builder.setTitle("Descarga");
+                builder.setMessage("¿Deseas guardar el archivo?");
+                builder.setCancelable(false).setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        URL urlDescarga;
+                        try {
+                            urlDescarga = new URL(url);
+                            new DescargarFichero().execute(urlDescarga);
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).setNegativeButton("Cancelar",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                builder.create().show();
+            }
+        });
+
+        navegador.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(ActividadPrincipal.this);
+                builder.setMessage(description).setPositiveButton("Aceptar", null).setTitle("onReceivedError");
+                builder.show();
+            }
+
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 dialogo = new ProgressDialog(ActividadPrincipal.this);
@@ -85,6 +150,7 @@ public class ActividadPrincipal extends AppCompatActivity{
                 dialogo.show();
                 btnDetener.setEnabled(true);
             }
+
             @Override
             public void onPageFinished(WebView view, String url) {
                 dialogo.dismiss();
@@ -102,15 +168,100 @@ public class ActividadPrincipal extends AppCompatActivity{
             }
         });
 
+        ActivityCompat.requestPermissions(ActividadPrincipal.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
     }
 
     public void detenerCarga(View v) {
         navegador.stopLoading();
     }
+
     public void irPaginaAnterior(View v) {
-        navegador.goBack();
+        if (navegador.canGoBack()) {
+            navegador.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
+
     public void irPaginaSiguiente(View v) {
         navegador.goForward();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(ActividadPrincipal.this, "Permiso denegado para escribir en el almacenamiento.", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+
+    private class DescargarFichero extends AsyncTask<URL, Integer, Long> {
+        private String mensaje;
+
+        @Override
+        protected Long doInBackground(URL... url) {
+            String urlDescarga = url[0].toString();
+            mensaje = "";
+            InputStream inputStream = null;
+            try {
+                URL direccion = new URL(urlDescarga);
+                HttpURLConnection conexion =
+                        (HttpURLConnection) direccion.openConnection();
+                if (conexion.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    inputStream = conexion.getInputStream();
+                    String fileName = android.os.Environment
+                            .getExternalStorageDirectory().getAbsolutePath() +
+                            "/descargas";
+                    File directorio = new File(fileName);
+                    directorio.mkdirs();
+                    File file = new File(directorio, urlDescarga.substring(
+                            urlDescarga.lastIndexOf("/"),
+                            (urlDescarga.indexOf("?") == -1 ?
+                                    urlDescarga.length() : urlDescarga.indexOf("?"))));
+                    FileOutputStream fileOutputStream = new
+                            FileOutputStream(file);
+                    byte[] buffer = new byte[1024];
+                    int len = 0;
+                    int bytesRead = -1;
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        fileOutputStream.write(buffer, 0, bytesRead);
+                    }
+                    fileOutputStream.close();
+                    inputStream.close();
+                    mensaje = "Guardado en: " + file.getAbsolutePath();
+                } else {
+                    throw new Exception(conexion.getResponseMessage());
+                }
+            } catch (Exception ex) {
+                mensaje = ex.getClass().getSimpleName() + " " + ex.getMessage();
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+            return (long) 0;
+        }
+
+        protected void onPostExecute(Long result) {
+            AlertDialog.Builder builder = new
+                    AlertDialog.Builder(ActividadPrincipal.this);
+            builder.setTitle("Descarga");
+            builder.setMessage(mensaje);
+            builder.setCancelable(true);
+            builder.create().show();
+        }
+
+    }
+
+
+
 }
